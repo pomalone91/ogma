0957b66be957adcb64dd69e5a01c06082e5d335b
pomalone91 <pomalone91@gmail.com>
2022-06-11 21:53:45 -0600

    Initial Commit

---
+++ ogma/ogma.h
@@ -0,0 +1,18 @@
+//
+//  ogma.h
+//  ogma
+//
+//  Created by Paul Malone on 6/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ogma.
+FOUNDATION_EXPORT double ogmaVersionNumber;
+
+//! Project version string for ogma.
+FOUNDATION_EXPORT const unsigned char ogmaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ogma/PublicHeader.h>
+
+
